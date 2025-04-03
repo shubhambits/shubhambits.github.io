955b2700ce5c77af1f3f2fd6340d928ef8d8a4d0
Shubham Agrawal <shubhamagrawal751+github@gmail.com>
2025-04-03 18:59:34 +0530

    Updated Index

--- src/components/CICDShowcase.tsx
+++ src/components/CICDShowcase.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import pipelineTemplate from '../assets/pipeline-template.yml?raw';
 
 const CICDShowcase: React.FC = () => {
   return (
@@ -26,51 +27,7 @@ const CICDShowcase: React.FC = () => {
       <div className="code-example">
         <h3>Sample Pipeline Template</h3>
         <pre>
-          <code>
-{`# Azure DevOps Pipeline Template
-parameters:
-  - name: buildConfiguration
-    type: string
-    default: 'Release'
-  - name: projectName
-    type: string
-    default: 'MyProject'
-
-variables:
-  solution: '$(projectName).sln'
-  buildPlatform: 'Any CPU'
-
-stages:
-  - stage: Build
-    displayName: 'Build Stage'
-    jobs:
-      - job: Build
-        displayName: 'Build Job'
-        pool:
-          vmImage: 'windows-latest'
-        steps:
-          - task: NuGetRestore@1
-            displayName: 'Restore NuGet packages'
-            inputs:
-              solution: '$(solution)'
-              feedsToUse: 'config'
-              nugetConfigPath: 'nuget.config'
-              
-          - task: VSBuild@1
-            displayName: 'Build solution'
-            inputs:
-              solution: '$(solution)'
-              platform: '$(buildPlatform)'
-              configuration: $(buildConfiguration)
-              
-          - task: VSTest@2
-            displayName: 'Run unit tests'
-            inputs:
-              testSelector: 'testSuite'
-              testSuite: '**/*.test.dll'
-              platform: '$(buildPlatform)'
-              configuration: $(buildConfiguration)`}
-          </code>
+          <code>{pipelineTemplate}</code>
         </pre>
       </div>
     </section>
